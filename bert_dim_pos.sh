4850f4d7eecafa44bf96b1bafc14e02eae8aced7
Camille <camille.barboule@gmail.com>
2025-06-24 16:39:26 +0200

    removal des fichiers tests non nécessaires

--- bert_dim_pos.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-source .venv/bin/activate
-pip install -e .
-pip install regex safetensors tokenizers wandb dotenv 'accelerate>=0.26.0'
-
-python3 tests/models/bert_dim_pos/test_training.py --model_name "test_bert_dim_pos"
