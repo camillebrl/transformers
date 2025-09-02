385e13fb1f6a07a94d3c0ccbacd04a6b95181394
LequeuISIR <lequeu@isir.upmc.fr>
2025-09-02 14:30:55 +0200

    fixed posbert pooler

--- src/transformers/models/posbert/modeling_bert.py
+++ src/transformers/models/posbert/modeling_bert.py
@@ -162,7 +162,7 @@ class SemBertEmbeddings(nn.Module):
         super().__init__()
         # print("initializing SemBertEmbeddings")
         self.word_embeddings = nn.Embedding(config.vocab_size, config.hidden_size, padding_idx=config.pad_token_id)
-
+        self.token_type_embeddings = nn.Embedding(config.type_vocab_size, config.hidden_size)
         # self.LayerNorm is not snake-cased to stick with TensorFlow model variable name and be able to load
         # any TensorFlow checkpoint file
         self.LayerNorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
@@ -171,11 +171,28 @@ class SemBertEmbeddings(nn.Module):
     def forward(
         self,
         input_ids: Optional[torch.LongTensor] = None,
+        token_type_ids: Optional[torch.LongTensor] = None,
         inputs_embeds: Optional[torch.FloatTensor] = None,
     ) -> torch.Tensor:
         # print("forward SemBertEmbeddings")
         if inputs_embeds is None:
             inputs_embeds = self.word_embeddings(input_ids)
+        
+        if input_ids is not None:
+            input_shape = input_ids.size()
+        else:
+            input_shape = inputs_embeds.size()[:-1]
+
+        seq_length = input_shape[1]
+
+        if token_type_ids is None:
+            if hasattr(self, "token_type_ids"):
+                buffered_token_type_ids = self.token_type_ids[:, :seq_length]
+                buffered_token_type_ids_expanded = buffered_token_type_ids.expand(input_shape[0], seq_length)
+                token_type_ids = buffered_token_type_ids_expanded
+            else:
+                token_type_ids = torch.zeros(input_shape, dtype=torch.long, device= input_ids.device if input_ids is not None else inputs_embeds.device)
+
 
         embeddings = inputs_embeds
         embeddings = self.LayerNorm(embeddings)
@@ -1209,7 +1226,7 @@ class PosBertModel(PosBertPreTrainedModel):
         return self.sem_embeddings.word_embeddings
 
     def set_input_embeddings(self, value):
-        self.embeddings.word_embeddings = value
+        self.sem_embeddings.word_embeddings = value
 
     def _prune_heads(self, heads_to_prune):
         """
@@ -1272,8 +1289,8 @@ class PosBertModel(PosBertPreTrainedModel):
         device = input_ids.device if input_ids is not None else inputs_embeds.device
 
         if token_type_ids is None:
-            if hasattr(self.embeddings, "token_type_ids"):
-                buffered_token_type_ids = self.embeddings.token_type_ids[:, :seq_length]
+            if hasattr(self.sem_embeddings, "token_type_ids"):
+                buffered_token_type_ids = self.sem_embeddings.token_type_ids[:, :seq_length]
                 buffered_token_type_ids_expanded = buffered_token_type_ids.expand(batch_size, seq_length)
                 token_type_ids = buffered_token_type_ids_expanded
             else:
